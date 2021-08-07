2bb5495715f1cce2722abb47fe94f8fd2653fd65
MiguelED <migueled2695@gmail.com>
2021-08-07 18:21:59 -0500

    Control flow

---
+++ scalarTypesOperatorsAndControlFlow/controlFlow.py
@@ -0,0 +1,28 @@
+if true:
+    print("It´s true!")
+
+if bool("eggs"):
+    print("yes please!")
+
+if "egss"
+    print("yes please!")
+
+if h > 50:
+    print("Greater than 50")
+else:
+    print("50 or smaller")
+
+if h > 50:
+    print("Greater than 50")
+else:
+    if h < 20:
+        print("less than 20")
+    else:
+        print("between 20 and 50")
+
+if h > 50:
+    print("Greater than 50")
+elif h < 20:
+    print("less than 20")
+else:
+    print("between 20 and 50")
