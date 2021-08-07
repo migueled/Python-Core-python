ed8ed8fb518d4db8af4bee23994262008672b018
MiguelED <migueled2695@gmail.com>
2021-08-07 18:03:41 -0500

    Scalar types

--- note.md
+++ note.md
@@ -10,4 +10,9 @@ fac(n) // ( fact(k) - fac(n - k) )
 2**31-1
 fact(13) > 2**31-1
 fact(100)
+
+_
+
+import this
+
 ```
---
+++ scalarTypesOperatorsAndControlFlow/types.py
@@ -0,0 +1,46 @@
+##int
+##unlimited precision signed integer
+
+10      ##Decimal
+0b10    ##binary
+0o10    ##octal
+0x10    ##hexadecimal
+int(3.5)
+int(-3.5)
+int("496")
+int("10000" , 3)
+
+##float
+##IEEE-754 double-precision with 53-bits f binary precision
+##15-16 significant digits in decimal
+
+3.125
+3e8
+1.61e-35
+float(7)
+float("1.618")
+float("nan")
+float("inf")
+float("-inf")
+3.0 + 1
+
+##None
+##null value
+##often represents the absence of a value
+Nonea = None
+a is None
+
+##bool
+##boolean logical values
+true
+false
+bool(0)
+bool(1)
+bool(42)
+bool(-1)
+bool(0.0)
+bool([])
+bool([1,5,5])
+bool("")
+bool("Spam")
+
