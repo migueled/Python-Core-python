dcffe1bc571bdfd03e0741bcdd7dd919b449812b
MiguelED <migueled2695@gmail.com>
2021-08-10 23:21:05 -0500

    list

---
+++ stringsCollectionsAndIteration/list.py
@@ -0,0 +1,11 @@
+##List
+##sequences of objects
+##mutable
+##a workhorse in python
+[ 1 , 2 , 3 ]
+a = ["apple", "orange", "pear"]
+a[1] = 7
+b = []
+b.append( 1.618 )
+b.append( 4.0 )
+list( "characters" )
