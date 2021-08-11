f6cec078944c2dc4d7ea3bd80a7813b783aa24e6
MiguelED <migueled2695@gmail.com>
2021-08-10 23:38:52 -0500

    for loops

---
+++ stringsCollectionsAndIteration/forLoop.py
@@ -0,0 +1,8 @@
+##for-loop
+##visit each item in a iterable sequence
+cities = [ 'London', 'New York', 'Paris', 'Oslo']
+for city in cities:
+    print(city)
+colors = { 'crimson' : 0xdc143c, 'coral': 0xff7f50, 'teal': 0x008080 }
+for color in colors
+    print( color , colors[color])
