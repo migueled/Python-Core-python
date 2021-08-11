af1ac66d1470377d29b5e99d3f2a3358c5e28712
MiguelED <migueled2695@gmail.com>
2021-08-10 23:29:12 -0500

    dict

---
+++ stringsCollectionsAndIteration/dict.py
@@ -0,0 +1,9 @@
+##dict
+##fundamental data structure in python
+##mep keys to values
+##also know as maps or associate arrays
+## { key1: value1, key2: value2 }
+d = { 'alice' : '899-7421-8280', 'bob' : '899-7421-8237', 'eve' : '899-4121-8280'}
+d['alice'] = '878-7421-8280'
+d
+e = {}
