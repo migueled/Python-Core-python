044d9afca03a4cf7f901e774664b6a56cbc8e018
Miguel Silvano <miguel.silvano@ikusi.com>
2021-12-25 23:34:31 -0600

    dunder name

--- modularity/words.py
+++ modularity/words.py
@@ -1,13 +1,17 @@
 from urllib.request import urlopen
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-storyWords = []
+def fetchWords():
+    story = urlopen('http://sixty-north.com/c/t.txt')
+    storyWords = []
 
-for line in story:
-    lineWords = line.decode('utf-8').split()
-    for word in lineWords:
-        storyWords.append(word)
-story.close
+    for line in story:
+        lineWords = line.decode('utf-8').split()
+        for word in lineWords:
+            storyWords.append(word)
+    story.close
 
-for word in storyWords:
-    print(word)
+    for word in storyWords:
+        print(word)
+
+if __name__ == '__main__':
+    fetchWords()
