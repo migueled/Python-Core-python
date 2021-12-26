4f0c3ae8284d8f91a8347b517016290c87740028
Miguel Silvano <miguel.silvano@ikusi.com>
2021-12-26 00:02:09 -0600

    docStrings

--- modularity/words.py
+++ modularity/words.py
@@ -1,8 +1,23 @@
 #python words.py http://sixty-north.com/c/t.txt
+#python
+#import words
+#help(words)
+"""retrieve and print words from a URL.
+Usage:
+    python3 words.py <url>
+"""
 import sys
 from urllib.request import urlopen
 
 def fetchWords(url):
+    """Fecth a list of words from a URL
+    Args:
+        url: the url of a UTF-8 text document.
+    
+    Returns:
+        a list of strings containing the words from
+        the document.
+    """
     story = urlopen(url)
     storyWords = []
     for line in story:
@@ -13,10 +28,18 @@ def fetchWords(url):
     return storyWords
 
 def printItems(items):
+    """Print items one per line
+    Args:
+        an iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """Print each word from a text documents from at a url
+    Args:
+        url: the url of a UTF-8 text document.
+    """
     words = fetchWords(url)
     printItems(words)
 
