214e2e27972bbbf0e5d090d253952f8ffe164469
Miguel Silvano <miguel.silvano@ikusi.com>
2021-12-25 23:50:02 -0600

    command line arguments

--- modularity/words.py
+++ modularity/words.py
@@ -1,17 +1,24 @@
+#python words.py http://sixty-north.com/c/t.txt
+import sys
 from urllib.request import urlopen
 
-def fetchWords():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+def fetchWords(url):
+    story = urlopen(url)
     storyWords = []
-
     for line in story:
         lineWords = line.decode('utf-8').split()
         for word in lineWords:
             storyWords.append(word)
     story.close
+    return storyWords
 
-    for word in storyWords:
-        print(word)
+def printItems(items):
+    for item in items:
+        print(item)
+
+def main(url):
+    words = fetchWords(url)
+    printItems(words)
 
 if __name__ == '__main__':
-    fetchWords()
+    main(sys.argv[1])
